4380d3d448fdaec96e31b3ce12cac706e836b9f4
Haronchick <logvinovdenharo@gmail.com>
2018-03-10 19:58:57 +0300

    solution task longerst-consecutive-sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function longestConsecutiveLength(array) {
   // your solution here
+    let longerSequensyKey;
+    let lengthArray = array.length, hashMap = {};
+
+    if (array.length === 0) {
+        return 0;
+    }
+
+    for (let i = 0; i < lengthArray; i++){
+      hashMap[array[i]] = [array[i]];
+
+      if (hashMap[array[i]+1] && hashMap[array[i]-1]) {
+          hashMap[array[i]] = [].concat(hashMap[array[i]-1],array[i],  hashMap[array[i]+1]);
+      } else if (hashMap[array[i]+1]) {
+          hashMap[array[i]] = [].concat(array[i], hashMap[array[i]+1]);
+      } else if (hashMap[array[i]-1]) {
+           hashMap[array[i]] = [].concat( hashMap[array[i]-1], array[i]);
+      }
+
+      if (longerSequensyKey === undefined || hashMap[array[i]].length > hashMap[longerSequensyKey].length) {
+          longerSequensyKey = array[i];
+      }
+    }
+
+     return hashMap[longerSequensyKey].length || 0;
 }
+
